a70eaad6015d09f426cb07c4a894b0d2478d83fb
PratamaAngga <anggashillnoir@gmail.com>
2025-05-13 15:01:53 +0700

    exp 2

--- SLLMain20.java
+++ SLLMain20.java
@@ -29,6 +29,16 @@ public class SLLMain20 {
         }
 
         sc.close();
+
+        System.out.println("data index 1 : ");
+        sll.getData(1);
+        System.out.println("data mahasiswa an Bimon berada pada index : "+sll.indexOf("Bimon"));
+        System.out.println();
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
     }
 }
 
